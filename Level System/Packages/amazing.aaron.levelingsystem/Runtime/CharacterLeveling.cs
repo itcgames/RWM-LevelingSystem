16712e49776f2a960d884990584ea254f37c157f
Aaron O Neill <c00241596@itcarlow.ie>
2021-11-07 00:01:51 +0000

    CharacterLevel ThresholdTest passing

---
+++ Level System/Packages/amazing.aaron.levelingsystem/Runtime/CharacterLeveling.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLeveling : MonoBehaviour
+{
+    int _level;
+    int _experience;
+    [SerializeField] int _levelThreshold;
+    [SerializeField] float _levelScaler;
+    [SerializeField] int _MAX_LEVEL = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _level = 1;
+        _experience = 0;
+    }
+
+    public void GainExperience(int t_experience)
+    {
+        _experience += t_experience;
+        if (_experience > _levelThreshold)
+        {
+            ++_level;
+            _experience -= _levelThreshold;
+            _levelThreshold = _levelThreshold + (int)(_levelThreshold * _levelScaler);
+        }
+    }
+
+    public void SetThreshold(int t_threshold)
+    {
+        _levelThreshold = t_threshold;
+    }
+
+    public int getLevel()
+    {
+        return _level;
+    }
+}
---
+++ Level System/Packages/amazing.aaron.levelingsystem/Tests/CharacterLevelingTests.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class CharacterLevelingTests
+{
+
+    private CharacterLeveling leveling;
+
+    [SetUp]
+    public void Setup()
+    {
+        GameObject gameGameObject =
+          MonoBehaviour.Instantiate(
+            Resources.Load<GameObject>("Prefabs/Game"));
+        GameObject player = gameGameObject.transform.Find("Player").gameObject;
+        if (player != null)
+            leveling = player.GetComponent<CharacterLeveling>();
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+    }
+
+    [UnityTest]
+    public IEnumerator ThresholdTest()
+    {
+        if (leveling != null)
+        {
+            Assert.True(1 == leveling.getLevel());
+            leveling.SetThreshold(100); 
+            leveling.GainExperience(101);
+            yield return new WaitForSeconds(0.1f);
+            Assert.Greater(leveling.getLevel(), 1);
+        }
+        yield return null;
+    }
+}
