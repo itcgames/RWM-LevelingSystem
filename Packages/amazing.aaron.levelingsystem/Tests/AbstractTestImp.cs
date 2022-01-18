a6e20246beb817479eab97ecb6fa25e6bdaac64a
Aaron O Neill <45260706+Aaron0Neill@users.noreply.github.com>
2022-01-18 17:53:55 +0000

    Feature Completed: Ability Interface [JIRA NOT SETUP]

---
+++ Packages/amazing.aaron.levelingsystem/Tests/AbstractTestImp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbstractTestImp : Ability
+{
+    private int _value =0;
+    public override void Execute(GameObject t_caller)
+    {
+        t_caller.GetComponent<AbstractTestImp>()._value = 1;
+    }
+
+    public int getValue()
+    {
+        return _value;
+    }
+}
---
+++ Packages/amazing.aaron.levelingsystem/Tests/AbstractTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class AbstractTests
+    {
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator AbstractTestsWithEnumeratorPasses()
+        {
+            GameObject g = new GameObject();
+            AbstractTestImp imp = g.AddComponent<AbstractTestImp>();
+            Ability ab = imp;
+            ab.Execute(g);
+            yield return new WaitForEndOfFrame();
+            Assert.AreNotEqual(0, imp.getValue());
+        }
+    }
+}
