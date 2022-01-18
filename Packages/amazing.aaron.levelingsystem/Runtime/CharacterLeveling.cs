5f1bd6d5a2519ff7d6021b3f99a53a2a2f580cf2
Aaron O Neill <45260706+Aaron0Neill@users.noreply.github.com>
2022-01-18 18:12:37 +0000

    Added experience tracking with threshold dropping [JIRA NOT SETUP]

---
+++ Packages/amazing.aaron.levelingsystem/Runtime/CharacterLeveling.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLeveling : MonoBehaviour
+{
+    private float _experience;
+    [SerializeField] private float _threshold;
+
+
+    public void addExperience(float t_experienceGained)
+    {
+        _experience += t_experienceGained;
+        if (_experience > _threshold)
+            _experience -= _threshold;
+    }
+
+    public void setThreshold(float t_threshold)
+    {
+        _threshold = t_threshold;
+    }
+
+    public float getExperience()
+    {
+        return _experience;
+    }
+
+    public float getThreshold()
+    {
+        return _threshold;
+    }
+}
---
+++ Packages/amazing.aaron.levelingsystem/Tests/CharacterLevelingTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CharacterLevelingTest
+    {
+        [UnityTest]
+        public IEnumerator GainExperience()
+        {
+            CharacterLeveling c = new CharacterLeveling();
+            c.addExperience(100);
+            yield return null;
+            Assert.Greater(c.getExperience(), 0);
+        }
+
+
+        [UnityTest]
+        public IEnumerator thresholdDrop()
+        {
+            CharacterLeveling c = new CharacterLeveling();
+            c.setThreshold(50);
+            c.addExperience(100);
+            yield return null;  
+            Assert.AreEqual(50, c.getExperience());
+        }
+    }
+}
