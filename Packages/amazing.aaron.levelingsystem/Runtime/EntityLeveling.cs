f4490f6f9c58420cc071ee972af69e01cbc7f51b
Aaron O Neill <45260706+Aaron0Neill@users.noreply.github.com>
2022-01-20 22:04:09 +0000

    Completed Task: Entity Level [IN-10]

---
+++ Packages/amazing.aaron.levelingsystem/Runtime/EntityLeveling.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityLeveling : MonoBehaviour
+{
+    int _level = 1;
+
+    public void levelUp()
+    {
+        _level++;
+    }
+
+    public int getLevel()
+    {
+        return _level;
+    }
+}
