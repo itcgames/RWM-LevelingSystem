d4d4b5b5e7a79c00c5ac9563ec9cc0d9cc0e5557
Aaron Home <C00241596@itcarlow.ie>
2022-01-20 22:02:19 +0000

    Completed Task: Entity Level

---
+++ Packages/amazing.aaron.levelingsystem/Tests/EntityLevelingTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class EntityLevelingTest
+    {
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator EntityLevelIncrease()
+        {
+            EntityLeveling e = new EntityLeveling();
+            e.levelUp();
+            yield return null;
+            Assert.AreNotEqual(1, e.getLevel());
+        }
+    }
+}
