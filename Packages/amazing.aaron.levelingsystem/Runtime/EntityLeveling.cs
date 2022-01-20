8e138f20820fbe898b525b33fe708d49590abd29
Aaron O Neill <45260706+Aaron0Neill@users.noreply.github.com>
2022-01-20 22:26:52 +0000

    Feature Complete: Entity Leveling [IN-2]
    * Completed Task: Entity Level [IN-10]
    
    * Completed Task: Max Level [IN-9]
    
    * Task Completed: Entity Abilities [IN-8]

---
+++ Packages/amazing.aaron.levelingsystem/Runtime/EntityLeveling.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityLeveling : MonoBehaviour
+{
+    int _level = 1;
+    [SerializeField] int _maxLevel = 10;
+    [SerializeField] Abilities[] _levelAbilities;
+
+    public void levelUp()
+    {
+        if (_level < _maxLevel)
+        {
+            _level++;
+            foreach (var pair in _levelAbilities)
+                if (pair._boundLevel == _level)
+                    pair._ability.Execute(gameObject);
+        }
+    }
+
+    public void setMaxLevel(int t_maxLevel)
+    {
+        _maxLevel = t_maxLevel;
+    }
+
+    public int getLevel()
+    {
+        return _level;
+    }
+}
---
+++ Packages/amazing.aaron.levelingsystem/Tests/EntityLevelingTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class EntityLevelingTest
+    {
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator EntityLevelIncrease()
+        {
+            EntityLeveling e = new EntityLeveling();
+            e.levelUp();
+            yield return null;
+            Assert.AreNotEqual(1, e.getLevel());
+        }
+
+        [UnityTest]
+        public IEnumerator MaxLevelCheck()
+        {
+            EntityLeveling e = new EntityLeveling();
+            e.setMaxLevel(1); // set max Level to 1
+            e.levelUp(); // try level up
+            yield return null;
+            Assert.AreEqual(1, e.getLevel()); // make sure i didnt exceed the max level
+        }
+    }
+}
