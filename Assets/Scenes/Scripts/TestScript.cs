30f3a066a890d80f912ed2b3b0fab909dbc4bca5
Aaron O Neill <45260706+Aaron0Neill@users.noreply.github.com>
2022-01-21 17:24:35 +0000

    Task Complete: Dynamic Foreground [IN-13]

--- Assets/Scenes/Scripts/TestScript.cs
+++ Assets/Scenes/Scripts/TestScript.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class TestScript : MonoBehaviour
 {
     public CharacterLeveling _level;
+<<<<<<< HEAD
     public int _expGain = 2;
+=======
+>>>>>>> 03e12f5 (Task Complete: Dynamic Foreground [IN-13])
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
+<<<<<<< HEAD
             _level.addExperience(_expGain);
+=======
+            _level.addExperience(10);
+>>>>>>> 03e12f5 (Task Complete: Dynamic Foreground [IN-13])
         }
     }
 }
