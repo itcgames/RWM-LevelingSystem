218351a283234415a0b1de01819bca841aacba52
Aaron Home <C00241596@itcarlow.ie>
2022-01-23 18:50:03 +0000

    Feature Complete: Level UI[IN-4]

--- Packages/amazing.aaron.levelingsystem/Runtime/Level_UI_Text.cs
+++ Packages/amazing.aaron.levelingsystem/Runtime/Level_UI_Text.cs
@@ -16,19 +16,35 @@ public class Level_UI_Text : MonoBehaviour
     public CharacterLeveling _character;
     public TextStyle _style;
 
+    void Update()
+    {
+        if (_style == TextStyle.NONE)
+            GetComponent<Text>().text = "";
+        else if (_style == TextStyle.PERCENTAGE)
+        {
+            string text = (_character.getPercentage() * 100f).ToString();
+            text += "%";
+            GetComponent<Text>().text = text;
+        }
+        else
+        {
+            string text = _character.getExperience().ToString();
+            GetComponent<Text>().text = text;
+        }
+    }
+
     void OnValidate()
     {
         if (_style == TextStyle.NONE)
             GetComponent<Text>().text = "";
         else if (_style == TextStyle.PERCENTAGE)
         {
-            string text = (_character.getPercentage() * 100.0f).ToString();
-            text += '%';
+            string text = "25%";
             GetComponent<Text>().text = text;
         }
         else
         {
-            string text = _character.getExperience().ToString();
+            string text = "0";
             GetComponent<Text>().text = text;
         }
     }
